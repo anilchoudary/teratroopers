639d92f6dc70b1175e70adad29d0659cc6d1201d
pvinusha <vinushareddy1997@gmail.com>
2017-08-26 14:28:24 +0530

    vinusha

--- app/src/main/java/teratroopers/companion/MainActivity.java
+++ app/src/main/java/teratroopers/companion/MainActivity.java
@@ -30,6 +30,7 @@ public class MainActivity extends AppCompatActivity
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
+                //vinusha
             }
         });
 
