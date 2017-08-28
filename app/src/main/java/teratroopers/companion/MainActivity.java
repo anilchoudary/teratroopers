63d296211eb5ddde231654ee5915d374609a7e46
anilchoudary <anilakku2@gmail.com>
2017-08-28 19:27:52 +0530

    Add a Class Commited

--- app/src/main/java/teratroopers/companion/MainActivity.java
+++ app/src/main/java/teratroopers/companion/MainActivity.java
@@ -1,5 +1,6 @@
 package teratroopers.companion;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -28,10 +29,7 @@ public class MainActivity extends AppCompatActivity
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-                //vinusha
-                //sravani
+
             }
         });
 
