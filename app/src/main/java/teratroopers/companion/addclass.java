ae0fc25ddb9c7d41f7deca77265ca735425601c2
imprashanthv <vissaprashanth@gmail.com>
2017-08-29 14:55:50 +0530

    support screen

--- app/src/main/java/teratroopers/companion/addclass.java
+++ app/src/main/java/teratroopers/companion/addclass.java
@@ -2,10 +2,14 @@ package teratroopers.companion;
 
 
 import android.os.Bundle;
+import android.os.Vibrator;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import static android.content.Context.VIBRATOR_SERVICE;
 
 
 /**
@@ -14,6 +18,7 @@ import android.view.ViewGroup;
 public class addclass extends Fragment {
 
 
+    Vibrator vibe;
     public addclass() {
         // Required empty public constructor
     }
@@ -23,6 +28,7 @@ public class addclass extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
+
         return inflater.inflate(R.layout.fragment_addclass, container, false);
     }
 
