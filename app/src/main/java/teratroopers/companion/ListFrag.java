d7ff5671d34efd17fbe475fc25bbec1e3b7054f0
imprashanthv <vissaprashanth@gmail.com>
2017-09-03 19:26:32 +0530

    //03/09 - Prashanth App no more crashes; Try to add listeners to buttons that'll take us to attendance taking activity;

--- app/src/main/java/teratroopers/companion/ListFrag.java
+++ app/src/main/java/teratroopers/companion/ListFrag.java
@@ -19,7 +19,8 @@ import android.widget.LinearLayout;
 public class ListFrag extends Fragment {
     // TODO: Rename parameter arguments, choose names that match
     View view;
-    mydbhelper mydb=new mydbhelper(getContext());
+    mydbhelper mydb;
+
     Button b1;
     LinearLayout linearLayout;
     int n,i;
@@ -36,6 +37,7 @@ public class ListFrag extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         view =inflater.inflate(R.layout.fragment_list2, container, false);
+        this.mydb = new mydbhelper((Context)this.getActivity());
         return view;
     }
 
@@ -47,81 +49,26 @@ public class ListFrag extends Fragment {
         //paste
 
         linearLayout=(LinearLayout)view.findViewById(R.id.linearlayout);
-        Cursor res = mydb.getalldata();
-        if (res.getCount() == 0) {
-            //show message
-            // showmessage("Error", "Nothing found");
-            return;
-        }
-        // StringBuffer buffer = new StringBuffer();
-        while (res.moveToNext()) {
-            n++;
-           /* buffer.append("ClassName :" + res.getString(0) + "\n");
-            buffer.append("Starting Roll :" + res.getString(1) + "\n");
-            buffer.append("Ending Roll :" + res.getString(2) + "\n");*/
-
-            while(res.moveToNext()){
-                names[i]=res.getString(0);
-                Log.i("class name: ",names[i]);
-                i++;
-            }
-
-        }
-       // names=new String[n];
-
-       // res.moveToFirst();
-
-
-        //show all data
-        //showmessage("Data", buffer.toString());
-        // createbuttonslist();
-
+        createbuttons();
     }
-    /*public void viewList(){
+    public void createbuttons() {
 
-        linearLayout=(LinearLayout)view.findViewById(R.id.linearlayout);
-        Cursor res = mydb.getalldata();
-        if (res.getCount() == 0) {
-            //show message
-           // showmessage("Error", "Nothing found");
-            return;
-        }
-       // StringBuffer buffer = new StringBuffer();
+        Cursor res=mydb.getalldata();
         while (res.moveToNext()) {
-            n++;
-           /* buffer.append("ClassName :" + res.getString(0) + "\n");
-            buffer.append("Starting Roll :" + res.getString(1) + "\n");
-            buffer.append("Ending Roll :" + res.getString(2) + "\n");
-
-        }
-        names=new String[n];
-
-        res.moveToFirst();
-        while(res.moveToNext()){
-            names[i]=res.getString(0);
-            Log.i("class name: ",names[i]);
-            i++;
-        }
-
-        //show all data
-        //showmessage("Data", buffer.toString());
-       // createbuttonslist();
-    }*/
-   /* public void showmessage(String title,String Message) {
-        AlertDialog.Builder builder = new  AlertDialog.Builder(getActivity());
-        builder.setCancelable(true);
-        builder.setTitle(title);
-        builder.setMessage(Message);
-        builder.show();
-
-    }*/
-   /* public void createbuttonslist(){
-        for(int i=0;i<n;i++) {
+            i=0;
             b1 = new Button(getActivity());
-            b1.setText(names[i]);
+            b1.setText(res.getString(0));
             linearLayout.addView(b1);
         }
-    }*/
+
+        /*for (i = 0; i < 5; i++) {
+            // b1=new Button(getContext());
+            b1 = new Button(getActivity());
+            b1.setText("manual");
+            linearLayout.addView(b1);
+        }*/
+    }
+
 }
 
 
